aca3f3a1385607d458804ed92ebe8df4c98f5648
grigorykulik <stacheldraht23@gmail.com>
2022-02-07 22:31:41 +0300

    no message

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0103/Solution.java
@@ -0,0 +1,12 @@
+package com.javarush.task.task01.task0103;
+
+/* 
+Мой юный друг
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int i=3126;
+        System.out.println(i-8);
+    }
+}
